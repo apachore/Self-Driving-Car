2be18abed5f8d2df5293d848e8faa7b17fe40c4b
Bhupendra Naphade <Bhupendra Naphade>
2015-10-26 10:41:49 -0700

    Corrected queue implementation

--- L5_Application/sensor.cpp
+++ L5_Application/sensor.cpp
@@ -29,19 +29,12 @@ int reading = 0,sum4 = 0, avg4= 0,pin_number;
 //QueueHandle_t sensor_data_q=0;
 void sensor_trig(int pin)
 {
-    static QueueHandle_t sensor_data_q = scheduler_task::getSharedObject("sensor_queue");
 
-    if (NULL == sensor_data_q) {
-        //light_up_the_project_blown_led();
-    }
-    else if (xQueueReceive(sensor_data_q, &pin_number, 0))
-    {
-    LPC_GPIO2->FIOPIN &= ~(1 << pin_number);
-      LPC_GPIO2->FIOPIN |= (1 << pin_number);
+      LPC_GPIO2->FIOPIN &= ~(1 << pin);
+      LPC_GPIO2->FIOPIN |= (1 << pin);
       delay_us(10);
-      LPC_GPIO2->FIOPIN &= ~(1 << pin_number);
+      LPC_GPIO2->FIOPIN &= ~(1 << pin);
       sensor_start_time = lpc_timer_get_value(lpc_timer0);
-    }
 }
 
 void Back_IR_read()
@@ -50,45 +43,48 @@ void Back_IR_read()
 
 }
 void sensor1_fall_edge()
-{     static QueueHandle_t sensor_data_q = scheduler_task::getSharedObject("sensor_queue");
+{
+        static QueueHandle_t sensor_data_q = scheduler_task::getSharedObject("sensor_queue");
         sensor1_echo_width = lpc_timer_get_value(lpc_timer0)-sensor_start_time;
         distance1 =  (sensor1_echo_width* 0.017) - 7;
         //sensor_trig(2);
         LE.on(1);
-        LE.off(3);
+        LE.off(2);
         pin_number =2;
-        if(!xQueueSend(sensor_data_q,&pin_number, 1))
+        if(!xQueueSend(sensor_data_q,&pin_number, 0))
         {
-                    puts("Failed to send item to queue");
-                }
+            LE.toggle(4);//   puts("Failed to send item to queue");
+         }
 }
 
 void sensor2_fall_edge()
-{ static QueueHandle_t sensor_data_q = scheduler_task::getSharedObject("sensor_queue");
-    sensor2_echo_width = lpc_timer_get_value(lpc_timer0)-sensor_start_time;
+{
+        static QueueHandle_t sensor_data_q = scheduler_task::getSharedObject("sensor_queue");
+        sensor2_echo_width = lpc_timer_get_value(lpc_timer0)-sensor_start_time;
         distance2 =  (sensor2_echo_width* 0.017) - 7;
         //sensor_trig(4);
         LE.on(2);
-              LE.off(1);
-        pin_number =4;
-                if(!xQueueSend(sensor_data_q,&pin_number, 1))
-                {
-                    puts("Failed to send item to queue");
-                        }
-}
-void sensor3_fall_edge()
-{ static QueueHandle_t sensor_data_q = scheduler_task::getSharedObject("sensor_queue");
-    sensor3_echo_width = lpc_timer_get_value(lpc_timer0)-sensor_start_time;
-        distance3 =  (sensor3_echo_width* 0.017) - 7;
-     //   sensor_trig(0);
-        LE.on(3);
-              LE.off(2);
+        LE.off(1);
         pin_number =0;
-                if(!xQueueSend(sensor_data_q,&pin_number, 1))
-                {
-                    puts("Failed to send item to queue");
-                        }
+        if(!xQueueSend(sensor_data_q,&pin_number, 0))
+        {
+            LE.toggle(4);//       puts("Failed to send item to queue");
+        }
 }
+//void sensor3_fall_edge()
+//{
+//        static QueueHandle_t sensor_data_q = scheduler_task::getSharedObject("sensor_queue");
+//        sensor3_echo_width = lpc_timer_get_value(lpc_timer0)-sensor_start_time;
+//        distance3 =  (sensor3_echo_width* 0.017) - 7;
+//     //   sensor_trig(0);
+//        LE.on(3);
+//        LE.off(2);
+//        pin_number =0;
+//        if(!xQueueSend(sensor_data_q,&pin_number, 0))
+//        {
+//            LE.toggle(4);//         puts("Failed to send item to queue");
+//        }
+//}
 
 
 sensorTask::sensorTask(uint8_t priority) : scheduler_task("sensorTask", 4*512, priority),
@@ -100,7 +96,7 @@ bool sensorTask::init(void)
         const uint8_t port2_1 = 1,port2_3 =3,port2_5 = 5;
         eint3_enable_port2(port2_1, eint_falling_edge, sensor1_fall_edge);
         eint3_enable_port2(port2_3, eint_falling_edge, sensor2_fall_edge);
-        eint3_enable_port2(port2_5, eint_falling_edge, sensor3_fall_edge);
+       // eint3_enable_port2(port2_5, eint_falling_edge, sensor3_fall_edge);
         LPC_PINCON->PINSEL3 |=  (3 << 28); // ADC-4 is on P1.30, select this as ADC0.4
 
         lpc_timer_enable(lpc_timer0,1);
@@ -108,7 +104,7 @@ bool sensorTask::init(void)
         LPC_GPIO2->FIODIR |=(1<<2);
         LPC_GPIO2->FIODIR |=(1<<4);
         //create queue
-        sensor_data_q = xQueueCreate(100, sizeof(uint32_t));
+        sensor_data_q = xQueueCreate(3, sizeof(int));
         addSharedObject("sensor_queue", sensor_data_q);
         sensor_trig(1);
         return (NULL != sensor_data_q);
@@ -116,9 +112,9 @@ bool sensorTask::init(void)
     }
 bool sensorTask::run(void *p)
     {
-    if( xQueueReceive(sensor_data_q, &pin_number,100))
+    if( xQueueReceive(sensor_data_q, &pin_number,1))
         {
-                  delay_us(mydel);
+        delay_us(mydel);
         sensor_trig(pin_number);
         }
     else{
@@ -127,13 +123,13 @@ bool sensorTask::run(void *p)
     printf("sensor1 distance = %d\n",distance1);
        printf("sensor2 distance = %d\n",distance2);
                 printf("sensor3 distance = %d\n",distance3);
-     //           delay_us(mydel);
+              // delay_us(mydel);
 //        sum1= sum1+distance1;
 //        sum2= sum2+distance2;
 //        sum3= sum3+distance3;
              //   Back_IR_read();
             //    printf("IR reading:%d\n",reading);
-                printf("IR reading:%d\n",reading);
+           //     printf("IR reading:%d\n",reading);
 //        count++;
 //        if (count == 10)
 //            {avg1= sum1/10;
